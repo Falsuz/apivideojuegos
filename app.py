e40e64228cd8c97b7ba7d5782f2d814d74189310
Samantha Falcon <valeria-samantha@hotmail.com>
2024-04-07 17:00:07 -0600

    septimo commiy

--- app.py
+++ app.py
@@ -1,80 +1,81 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://videogames_x44n_user:CCFK749Mn5CGtOMYnrWdp7lbDJ6lFyDk@dpg-co9huv4f7o1s739948s0-a.oregon-postgres.render.com/videogames_x44n'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Datos de ejemplo
-videojuegos = [
-    {
-        "id": 1,
-        "titulo": "Super Mario Bros.",
-        "desarrollador": "Nintendo",
-        "anio_lanzamiento": 1985,
-        "plataforma": "Nintendo Entertainment System",
-        "clasificacion": "E (Everyone)",
-        "url": "https://i.blogs.es/638318/super_mario_bros_logo/450_1000.webp"
-    },
-    {
-        "id": 2,
-        "titulo": "The Legend of Zelda: Ocarina of Time",
-        "desarrollador": "Nintendo",
-        "anio_lanzamiento": 1998,
-        "plataforma": "Nintendo 64",
-        "clasificacion": "E (Everyone)",
-        "url": "https://upload.wikimedia.org/wikipedia/en/5/57/The_Legend_of_Zelda_Ocarina_of_Time.jpg"
-    },
-    {
-        "id": 3,
-        "titulo": "Final Fantasy VII",
-        "desarrollador": "Square Enix",
-        "anio_lanzamiento": 1997,
-        "plataforma": "PlayStation",
-        "clasificacion": "T (Teen)",
-        "url": "https://upload.wikimedia.org/wikipedia/en/c/c2/Final_Fantasy_VII_Box_Art.jpg"
-    },
-    {
-        "id": 4,
-        "titulo": "The Elder Scrolls V: Skyrim",
-        "desarrollador": "Bethesda Game Studios",
-        "anio_lanzamiento": 2011,
-        "plataforma": "PC, PlayStation 3, Xbox 360",
-        "clasificacion": "M (Mature)",
-        "url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.imdb.com%2Ftitle%2Ftt1814884%2Fmediaviewer%2Frm778924288&psig=AOvVaw1N5x17xNv-fHRlloQcPpFz&ust=1712440566881000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjv07KIrIUDFQAAAAAdAAAAABAE"
-    },
-    {
-        "id": 5,
-        "titulo": "Grand Theft Auto V",
-        "desarrollador": "Rockstar North",
-        "anio_lanzamiento": 2013,
-        "plataforma": "PC, PlayStation 3, PlayStation 4, Xbox 360, Xbox One",
-        "clasificacion": "M (Mature)",
-        "url": "https://m.media-amazon.com/images/I/916T5H6sCtL.jpg"
+db = SQLAlchemy(app)
+
+class Videojuego(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(120), nullable=False)
+    desarrollador = db.Column(db.String(120), nullable=False)
+    anioLanzamiento = db.Column(db.String(120), nullable=False)
+    plataforma = db.Column(db.String(120), nullable=False)
+    clasificacion = db.Column(db.String(120), nullable=False)
+    urlImagen = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<Videojuego {self.titulo}>"
+
+@app.route('/create', methods=['GET'])
+def create_table():
+    db.create_all()
+    return 'Tabla creada'
+
+# en esta estructura devuelvo los videojuegos existentes
+@app.route('/videojuegos', methods=['GET'])
+def get_videojuegos():
+    videojuegos = Videojuego.query.all()
+    videojuegos_list = []
+    for juego in videojuegos:
+        videojuego_data = {
+            'id': juego.id,
+            'titulo': juego.titulo,
+            'desarrollador': juego.desarrollador,
+            'anio_lanzamiento': juego.anioLanzamiento,
+            'plataforma': juego.plataforma,
+            'clasificacion': juego.clasificacion,
+            'urlImagen': juego.urlImagen
+        }
+        videojuegos_list.append(videojuego_data)
+    return jsonify({"videojuegos": videojuegos_list})
+
+@app.route('/videojuegos/<int:id>', methods=['GET'])
+def get_videojuego(id):
+    videojuego = Videojuego.query.get_or_404(id)
+    videojuego_data = {
+        'id': videojuego.id,
+        'titulo': videojuego.titulo,
+        'desarrollador': videojuego.desarrollador,
+        'anio_lanzamiento': videojuego.anioLanzamiento,
+        'plataforma': videojuego.plataforma,
+        'clasificacion': videojuego.clasificacion,
+        'urlImagen': videojuego.urlImagen
     }
-]
+    return jsonify({"videojuego": videojuego_data})
 
-# Ruta para obtener todos los videojuegos
-@app.route('/api/videojuegos', methods=['GET'])
-def obtener_videojuegos():
-    return jsonify({"videojuegos": videojuegos})
+@app.route('/videojuegos', methods=['POST'])
+def add_videojuego():
+    data = request.json
+    if not all(key in data for key in ['titulo', 'desarrollador', 'anio_lanzamiento', 'plataforma', 'clasificacion', 'urlImagen']):
+        abort(400, 'Faltan campos en la solicitud')
+    videojuego = Videojuego(titulo=data['titulo'], desarrollador=data['desarrollador'], anioLanzamiento=data['anio_lanzamiento'], plataforma=data['plataforma'], clasificacion=data['clasificacion'], urlImagen=data['urlImagen'])
+    db.session.add(videojuego)
+    db.session.commit()
+    return jsonify({'message': 'Videojuego agregado correctamente'}), 201
 
-# Ruta para obtener un videojuego por su ID
-@app.route('/api/videojuegos/<int:videojuego_id>', methods=['GET'])
-def obtener_videojuego(videojuego_id):
-    juego = next((juego for juego in videojuegos if juego["id"] == videojuego_id), None)
-    if juego:
-        return jsonify({"videojuego": juego})
-    else:
-        return jsonify({"mensaje": "Videojuego no encontrado"}), 404
-
-# Ruta para agregar un nuevo videojuego
-@app.route('/api/videojuegos', methods=['POST'])
-def agregar_videojuego():
-    nuevo_videojuego = request.json
-    videojuegos.append(nuevo_videojuego)
-    return jsonify({"mensaje": "Videojuego agregado correctamente"}), 201
+@app.route('/videojuegos/<int:id>', methods=['DELETE'])
+def delete_videojuego(id):
+    videojuego = Videojuego.query.get_or_404(id)
+    db.session.delete(videojuego)
+    db.session.commit()
+    return jsonify({'message': 'Videojuego eliminado correctamente'}), 200
 
 @app.route('/')
 def index():
-    return 'api videojuegos'
+    return 'API Videojuegos'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
--- videojuegos.py
+++ videojuegos.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, abort
 import requests
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -12,19 +11,45 @@ def index():
     return render_template('index.html', videojuegos=videojuegos)
 
 
+@app.route('/videojuego/<int:videojuego_id>')
+def obtener_videojuego_por_id(videojuego_id):
+    # Obtener el videojuego por su ID
+    videojuego = obtener_videojuego_api(videojuego_id)
+    # print(videojuego)
+    if videojuego:
+        # Renderizar la plantilla HTML del videojuego con los datos
+        return render_template('videojuego.html', videojuego=videojuego)
+    else:
+        # Si no se encuentra el videojuego, devolver un error 404
+        abort(404)
+
+
 def obtener_videojuegos_api():
     # Hacer una solicitud GET a tu API en Render
-    url = 'https://apivideojuegos.onrender.com/api/videojuegos'
+    url = 'https://apivideojuegos.onrender.com/videojuegos'
     respuesta = requests.get(url)
 
     # Verificar si la solicitud fue exitosa (código de estado 200)
     if respuesta.status_code == 200:
         # Devolver los datos de la API en formato JSON
-        return respuesta.json()
+        return respuesta.json()['videojuegos']
     else:
-        # Si la solicitud no fue exitosa, devolver un mensaje de error
+        # Si la solicitud no fue exitosa, devolver una lista vacía
         return []
 
+
+def obtener_videojuego_api(videojuego_id):
+    # Hacer una solicitud GET a tu API en Render para obtener el videojuego por su ID
+    url = f'https://apivideojuegos.onrender.com/videojuegos/{videojuego_id}'
+    respuesta = requests.get(url)
+
+    # Verificar si la solicitud fue exitosa (código de estado 200)
+    if respuesta.status_code == 200:
+        # Devolver los datos de la API en formato JSON
+        return respuesta.json()['videojuego']
+    else:
+        # Si la solicitud no fue exitosa, devolver None
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
